karlhiramoto/angular-seed

feat(tools): do not invoke tasks if not required
In dev executes tasks only when a specific file type has changed.
Fix #1432.

tools/tasks/assets_task.ts
@@ -1,0 +1,10 @@
+import { Task } from './task';
+
+export abstract class AssetsTask extends Task {
+  precondition(files: String[]) {
+    return files.reduce((a, f) => {
+      return a || (!f.endsWith('.css') && !f.endsWith('.sass') &&
+		   !f.endsWith('.scss') && !f.endsWith('.ts'));
+    }, false);
+  }
+}

tools/tasks/seed/build.js.dev.ts
@@ -6,6 +6,7 @@ import { join/*, sep, relative*/ } from 'path';
 
 import Config from '../../config';
 import { makeTsProject, templateLocals } from '../../utils';
+import { TypeScriptTask } from '../typescript_task';
 
 const plugins = <any>gulpLoadPlugins();
 
@@ -17,60 +18,64 @@ let typedBuildCounter = Config.TYPED_COMPILE_INTERVAL; // Always start with the 
  * Executes the build process, transpiling the TypeScript files (except the spec and e2e-spec files) for the development
  * environment.
  */
-export = () => {
-  let tsProject: any;
-  let typings = gulp.src([
-    Config.TOOLS_DIR + '/manual_typings/**/*.d.ts'
-  ]);
-  let src = [
-    join(Config.APP_SRC, '**/*.ts'),
-    '!' + join(Config.APP_SRC, '**/*.spec.ts'),
-    '!' + join(Config.APP_SRC, '**/*.e2e-spec.ts'),
-    '!' + join(Config.TMP_DIR, `**/${Config.NG_FACTORY_FILE}.ts`)
-  ];
+export =
+  class BuildJsDev extends TypeScriptTask {
+    run() {
+      let tsProject: any;
+      let typings = gulp.src([
+        Config.TOOLS_DIR + '/manual_typings/**/*.d.ts'
+      ]);
+      let src = [
+        join(Config.APP_SRC, '**/*.ts'),
+        '!' + join(Config.APP_SRC, '**/*.spec.ts'),
+        '!' + join(Config.APP_SRC, '**/*.e2e-spec.ts'),
+        '!' + join(Config.TMP_DIR, `**/${Config.NG_FACTORY_FILE}.ts`)
+      ];
 
-  let projectFiles = gulp.src(src);
-  let result: any;
-  let isFullCompile = true;
+      let projectFiles = gulp.src(src);
+      let result: any;
+      let isFullCompile = true;
 
-  // Only do a typed build every X builds, otherwise do a typeless build to speed things up
-  if (typedBuildCounter < Config.TYPED_COMPILE_INTERVAL) {
-    isFullCompile = false;
-    tsProject = makeTsProject({isolatedModules: true});
-    projectFiles = projectFiles.pipe(plugins.cached());
-    util.log('Performing typeless TypeScript compile.');
-  } else {
-    tsProject = makeTsProject();
-    projectFiles = merge(typings, projectFiles);
-  }
-
-  result = projectFiles
-    .pipe(plugins.plumber())
-    .pipe(plugins.sourcemaps.init())
-    .pipe(tsProject())
-    .on('error', () => {
-      typedBuildCounter = Config.TYPED_COMPILE_INTERVAL;
-    });
-
-  if (isFullCompile) {
-    typedBuildCounter = 0;
-  } else {
-    typedBuildCounter++;
-  }
-
-  return result.js
-    .pipe(plugins.sourcemaps.write())
-// Use for debugging with Webstorm/IntelliJ
-// https://github.com/mgechev/angular2-seed/issues/1220
-//    .pipe(plugins.sourcemaps.write('.', {
-//      includeContent: false,
-//      sourceRoot: (file: any) =>
-//        relative(file.path, PROJECT_ROOT + '/' + APP_SRC).replace(sep, '/') + '/' + APP_SRC
-//    }))
-    .pipe(plugins.template(Object.assign(
-      templateLocals(), {
-        SYSTEM_CONFIG_DEV: jsonSystemConfig
+      // Only do a typed build every X builds, otherwise do a typeless build to speed things up
+      if (typedBuildCounter < Config.TYPED_COMPILE_INTERVAL) {
+        isFullCompile = false;
+        tsProject = makeTsProject({isolatedModules: true});
+        projectFiles = projectFiles.pipe(plugins.cached());
+        util.log('Performing typeless TypeScript compile.');
+      } else {
+        tsProject = makeTsProject();
+        projectFiles = merge(typings, projectFiles);
       }
-     )))
-    .pipe(gulp.dest(Config.APP_DEST));
-};
+
+      result = projectFiles
+        .pipe(plugins.plumber())
+        .pipe(plugins.sourcemaps.init())
+        .pipe(tsProject())
+        .on('error', () => {
+          typedBuildCounter = Config.TYPED_COMPILE_INTERVAL;
+        });
+
+      if (isFullCompile) {
+        typedBuildCounter = 0;
+      } else {
+        typedBuildCounter++;
+      }
+
+      return result.js
+        .pipe(plugins.sourcemaps.write())
+    // Use for debugging with Webstorm/IntelliJ
+    // https://github.com/mgechev/angular2-seed/issues/1220
+    //    .pipe(plugins.sourcemaps.write('.', {
+    //      includeContent: false,
+    //      sourceRoot: (file: any) =>
+    //        relative(file.path, PROJECT_ROOT + '/' + APP_SRC).replace(sep, '/') + '/' + APP_SRC
+    //    }))
+        .pipe(plugins.template(Object.assign(
+          templateLocals(), {
+            SYSTEM_CONFIG_DEV: jsonSystemConfig
+          }
+         )))
+        .pipe(gulp.dest(Config.APP_DEST));
+      }
+  };
+

tools/tasks/task.ts
@@ -1,0 +1,25 @@
+/**
+ * Base class for all tasks.
+ */
+export abstract class Task {
+  /**
+   * Override this task if you want to implement some custom
+   * task activation mechanism. By default each task will be always executed.
+   *
+   * @param {string[]} files A list of files changed since the previous watch.
+   */
+  precondition(files: string[]): boolean {
+    return true;
+  }
+
+  /**
+   * Implements your task behavior.
+   *
+   * @param {any} done A function which should be activated once your task completes.
+   * @return {ReadWriteStream | Promise<any> | void} This method can either return a promise
+   * which should be resolved once your task execution completes, a stream
+   * which should throw an end event once your task execution completes
+   * or nothing in case you will manually invoke the `done` method.
+   */
+  abstract run(done?: any): any | Promise<any> | void;
+}

tools/tasks/typescript_task.ts
@@ -1,0 +1,9 @@
+import { Task } from './task';
+
+export abstract class TypeScriptTask extends Task {
+  precondition(files: String[]) {
+    return files.reduce((a, f) => {
+      return a || f.endsWith('.ts');
+    }, false);
+  }
+}

domrachevv/test

tools/tasks/assets_task.ts
@@ -1,0 +1,10 @@
+import { Task } from './task';
+
+export abstract class AssetsTask extends Task {
+  shallRun(files: String[]) {
+    return files.reduce((a, f) => {
+      return a || (!f.endsWith('.css') && !f.endsWith('.sass') &&
+       !f.endsWith('.scss') && !f.endsWith('.ts'));
+    }, false);
+  }
+}

tools/tasks/seed/build.js.dev.ts
@@ -1,3 +1,4 @@
+
 import * as gulp from 'gulp';
 import * as gulpLoadPlugins from 'gulp-load-plugins';
 import * as merge from 'merge-stream';
@@ -6,6 +7,7 @@ import { join/*, sep, relative*/ } from 'path';
 
 import Config from '../../config';
 import { makeTsProject, templateLocals } from '../../utils';
+import { TypeScriptTask } from '../typescript_task';
 
 const plugins = <any>gulpLoadPlugins();
 
@@ -17,60 +19,64 @@ let typedBuildCounter = Config.TYPED_COMPILE_INTERVAL; // Always start with the 
  * Executes the build process, transpiling the TypeScript files (except the spec and e2e-spec files) for the development
  * environment.
  */
-export = () => {
-  let tsProject: any;
-  let typings = gulp.src([
-    Config.TOOLS_DIR + '/manual_typings/**/*.d.ts'
-  ]);
-  let src = [
-    join(Config.APP_SRC, '**/*.ts'),
-    '!' + join(Config.APP_SRC, '**/*.spec.ts'),
-    '!' + join(Config.APP_SRC, '**/*.e2e-spec.ts'),
-    '!' + join(Config.APP_SRC, `**/${Config.NG_FACTORY_FILE}.ts`)
-  ];
+export =
+  class BuildJsDev extends TypeScriptTask {
+    run() {
+      let tsProject: any;
+      let typings = gulp.src([
+        Config.TOOLS_DIR + '/manual_typings/**/*.d.ts'
+      ]);
+      let src = [
+        join(Config.APP_SRC, '**/*.ts'),
+        '!' + join(Config.APP_SRC, '**/*.spec.ts'),
+        '!' + join(Config.APP_SRC, '**/*.e2e-spec.ts'),
+        '!' + join(Config.APP_SRC, `**/${Config.NG_FACTORY_FILE}.ts`)
+      ];
 
-  let projectFiles = gulp.src(src);
-  let result: any;
-  let isFullCompile = true;
+      let projectFiles = gulp.src(src);
+      let result: any;
+      let isFullCompile = true;
 
-  // Only do a typed build every X builds, otherwise do a typeless build to speed things up
-  if (typedBuildCounter < Config.TYPED_COMPILE_INTERVAL) {
-    isFullCompile = false;
-    tsProject = makeTsProject({isolatedModules: true});
-    projectFiles = projectFiles.pipe(plugins.cached());
-    util.log('Performing typeless TypeScript compile.');
-  } else {
-    tsProject = makeTsProject();
-    projectFiles = merge(typings, projectFiles);
-  }
-
-  result = projectFiles
-    .pipe(plugins.plumber())
-    .pipe(plugins.sourcemaps.init())
-    .pipe(tsProject())
-    .on('error', () => {
-      typedBuildCounter = Config.TYPED_COMPILE_INTERVAL;
-    });
-
-  if (isFullCompile) {
-    typedBuildCounter = 0;
-  } else {
-    typedBuildCounter++;
-  }
-
-  return result.js
-    .pipe(plugins.sourcemaps.write())
-// Use for debugging with Webstorm/IntelliJ
-// https://github.com/mgechev/angular2-seed/issues/1220
-//    .pipe(plugins.sourcemaps.write('.', {
-//      includeContent: false,
-//      sourceRoot: (file: any) =>
-//        relative(file.path, PROJECT_ROOT + '/' + APP_SRC).replace(sep, '/') + '/' + APP_SRC
-//    }))
-    .pipe(plugins.template(Object.assign(
-      templateLocals(), {
-        SYSTEM_CONFIG_DEV: jsonSystemConfig
+      // Only do a typed build every X builds, otherwise do a typeless build to speed things up
+      if (typedBuildCounter < Config.TYPED_COMPILE_INTERVAL) {
+        isFullCompile = false;
+        tsProject = makeTsProject({isolatedModules: true});
+        projectFiles = projectFiles.pipe(plugins.cached());
+        util.log('Performing typeless TypeScript compile.');
+      } else {
+        tsProject = makeTsProject();
+        projectFiles = merge(typings, projectFiles);
       }
-     )))
-    .pipe(gulp.dest(Config.APP_DEST));
-};
+
+      result = projectFiles
+        .pipe(plugins.plumber())
+        .pipe(plugins.sourcemaps.init())
+        .pipe(tsProject())
+        .on('error', () => {
+          typedBuildCounter = Config.TYPED_COMPILE_INTERVAL;
+        });
+
+      if (isFullCompile) {
+        typedBuildCounter = 0;
+      } else {
+        typedBuildCounter++;
+      }
+
+      return result.js
+        .pipe(plugins.sourcemaps.write())
+        // Use for debugging with Webstorm/IntelliJ
+        // https://github.com/mgechev/angular2-seed/issues/1220
+        //    .pipe(plugins.sourcemaps.write('.', {
+        //      includeContent: false,
+        //      sourceRoot: (file: any) =>
+        //        relative(file.path, PROJECT_ROOT + '/' + APP_SRC).replace(sep, '/') + '/' + APP_SRC
+        //    }))
+        .pipe(plugins.template(Object.assign(
+          templateLocals(), {
+            SYSTEM_CONFIG_DEV: jsonSystemConfig
+          }
+         )))
+        .pipe(gulp.dest(Config.APP_DEST));
+      }
+  };
+

tools/tasks/task.ts
@@ -1,0 +1,25 @@
+/**
+ * Base class for all tasks.
+ */
+export abstract class Task {
+  /**
+   * Override this task if you want to implement some custom
+   * task activation mechanism. By default each task will be always executed.
+   *
+   * @param {string[]} files A list of files changed since the previous watch.
+   */
+  shallRun(files: string[]): boolean {
+    return true;
+  }
+
+  /**
+   * Implements your task behavior.
+   *
+   * @param {any} done A function which should be activated once your task completes.
+   * @return {ReadWriteStream | Promise<any> | void} This method can either return a promise
+   * which should be resolved once your task execution completes, a stream
+   * which should throw an end event once your task execution completes
+   * or nothing in case you will manually invoke the `done` method.
+   */
+  abstract run(done?: any): any | Promise<any> | void;
+}

tools/tasks/typescript_task.ts
@@ -1,0 +1,9 @@
+import { Task } from './task';
+
+export abstract class TypeScriptTask extends Task {
+  shallRun(files: String[]) {
+    return files.reduce((a, f) => {
+      return a || f.endsWith('.ts');
+    }, false);
+  }
+}