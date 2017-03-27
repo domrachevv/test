72b68081700046696a2e9bfd235e5ff110ea7fac
artem-zhivoderov <artem.zhivoderov@returnonintelligence.com>
2017-03-27 17:19:06 +0300

    some configuration improvements + README.md updated

--- src/server/config/server.config.ts
+++ src/server/config/server.config.ts
@@ -1,11 +1,11 @@
 import { argv } from 'yargs';
 
 class ServerConfig {
-  MONGO_CONNECTION = argv['mongo_connection'] || process.env.MBCDEMO_MONGO_CONNECTION || 'mongodb://localhost/mongotest';
+  MONGO_CONNECTION = argv['mongo_connection'] || process.env.MONGO_CONNECTION || 'mongodb://localhost/mongotest';
 
-  PORT = argv['port'] || process.env.MBCDEMO_PORT || 9001;
+  PORT = argv['port'] || process.env.PORT || 9001;
 
-  BUILD_TYPE = (argv['build-type'] || argv['env'] || process.env.MBCDEMO_ENV || '').toLowerCase();
+  BUILD_TYPE = (argv['build-type'] || argv['env'] || process.env.ENV || '').toLowerCase();
 }
 
 const config: ServerConfig = new ServerConfig();
